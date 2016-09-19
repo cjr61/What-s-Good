04e84be43a5c11b14f2a3fb0d123de2baf3bb847
cjr61 <cjr61@zips.uakron.edu>
2016-09-18 22:18:27 -0400

    First Commit
    Working on camera background

---
+++ WhatsGood/WhatsGood/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  WhatsGood
+//
+//  Created by Cameron Reilly on 9/18/16.
+//  Copyright © 2016 Cameron Reilly. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var captureSession : AVCaptureSession?
+    var cameraLayer : AVCaptureVideoPreviewLayer?
+    
+    @IBOutlet weak var cameraView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080         //POSSIBLE CHANGES TO RESOULTION HERE
+        
+        var backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+    }
+}
+
